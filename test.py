9e6283cad960a8195fef9bc40d8d5029376eb3a6
Felipe Correa <pecolps@outlook.com>
2021-12-09 21:00:19 -0300

    Add little test

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ from pysintegra.processamento import ArquivoMagnetico
 arq = ArquivoMagnetico()
 arq.add_registro_10('12345678910110', 'ISENTO', 'Nome do Contribuinte / Razao Social',
                     'Rio de Janeiro', 'RJ', 'ISENTO', datetime.now(), datetime.now(), '1', '1', '1')
-
+arq.add_registro_90('12345678910110', 'ISENTO', '54', '1', '1')
 # TESTE COM SUCESSO
 print(arq.gerar())
